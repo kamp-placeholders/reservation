5c0a30fff54ef13dbe8647c7a59e11ef10e8d87b
paul-do <pauldo247@gmail.com>
2019-02-07 08:44:46 -0800

    Initial project set-up

---
+++ client/components/Reservation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Reservation extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        Hello from React!
+      </div>
+    )
+  }
+};
+
+export default Reservation;
